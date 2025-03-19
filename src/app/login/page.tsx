5c616b4718828fa5d412831a400354c2741eff3a
BlackKey <553180854@qq.com>
2025-03-20 00:29:46 +0800

    安装hono和prisma 并且连接本地mysql

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useRouter } from "next-nprogress-bar";
+import signPic from "@/public/sign-pic.png";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import { useState } from "react";
 import { Input } from "antd";
 import {
@@ -43,6 +45,7 @@ export default function Login() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
+          {/* 记住密码 */}
           <div></div>
           <button
             className="text-white flex justify-center items-center text-[17px] border-none cursor-pointer h-[53px] w-full bg-[#0c21c1] rounded-[32px]"
@@ -52,7 +55,18 @@ export default function Login() {
           </button>
         </div>
       </div>
-      <div className="flex-1 h-full rounded-[16px] border-1 border-solid border-black bg-[#000842]"></div>
+      <div className="max-w-[735px] flex-1  h-full rounded-[16px] border-1 border-solid border-black bg-[#000842]">
+        <Image
+          className="text-white mt-[64px] mx-auto"
+          width={521}
+          height={521}
+          quality={100}
+          src={signPic}
+          // 因为图片很大，所以优先加载图片
+          priority
+          alt="登录图片"
+        />
+      </div>
     </div>
   );
 }
